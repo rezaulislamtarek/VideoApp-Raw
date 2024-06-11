c33334acd29d92e88bf8db8ccd42cca51ae67a5d
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-06-11 11:33:17 +0600

    Initial Commit

---
+++ VideoApp Raw/VideoApp_RawApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoApp_RawApp.swift
+//  VideoApp Raw
+//
+//  Created by Rezaul Islam on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct VideoApp_RawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
